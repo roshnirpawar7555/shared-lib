12143e025e7953d03706e86408333c04fa07bea4
roshnirpawar7555 <74190168+roshnirpawar7555@users.noreply.github.com>
2021-05-03 21:43:47 +0530

    Update sed.groovy

--- vars/sed.groovy
+++ vars/sed.groovy
@@ -1,3 +1,9 @@
+def call(body) 
+{	
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 pipeline
 {
  agent any
@@ -17,4 +23,5 @@ cat token
       }
    }
   }
+}
 }
