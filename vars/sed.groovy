925ec751db252ef132b7ebe07441e181a01d5b08
roshnirpawar7555 <74190168+roshnirpawar7555@users.noreply.github.com>
2021-05-03 21:42:06 +0530

    Update sed.groovy

--- vars/sed.groovy
+++ vars/sed.groovy
@@ -1,1 +1,20 @@
-pipline 
+pipeline
+{
+ agent any
+   stages
+  {
+   stage('replace')
+   {
+    steps
+      {
+       script
+        {
+sh """
+sed 's/roshni/ruhi/' token
+cat token
+"""
+        }
+      }
+   }
+  }
+}
