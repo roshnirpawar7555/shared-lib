ff4c4617e6f1cc3e8ecaf37b5ada67e2dd0204ae
roshnirpawar7555 <74190168+roshnirpawar7555@users.noreply.github.com>
2021-05-05 14:52:22 +0530

    Update and rename with_credentials to with_credentials.groovy

---
+++ vars/with_credentials.groovy
@@ -0,0 +1,28 @@
+def call(body) 
+{	
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+pipeline
+{
+ agent any
+   stages
+  {
+   stage('replace')
+   {
+    steps
+      {
+       script
+        {
+sh """
+withCredentials([usernamePassword(credentialsId: 'my_password', passwordVariable: 'pass', usernameVariable: 'user')]) {
+    // some block
+}
+"""
+        }
+      }
+   }
+  }
+}
+}
